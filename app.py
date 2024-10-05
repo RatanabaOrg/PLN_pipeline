33a87a969da1febad84fe73fb356fea05bd7f3dd
Desduh <eduardofalandess@gmail.com>
2024-10-04 22:08:19 -0300

    adicao de app com rotas e de pipeline de pln em models

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-from models.handling_useless_tokens import handling_useless_tokens
-from models.replace_abbreviations import replace_abbreviations_in_text
+from flask import Flask, jsonify
+from models.pipeline_pnl import pipeline_pln
 
-# data scraping a
+app = Flask(__name__)
 
-# cleaned words stiles b
+@app.route('/route/<route>')
+def usuario(route):
+    pipeline_pln(route)
+    return jsonify({'mensagem': f'Olá, {route}!'})
 
-# tokenizing sentences and word tokens c
-word_tokens_en = ['I\'m', 'heading', 'to', 'the', 'gym', 'ASAP', ',', 'but', 'I\'ll', 'be', 'back', 'before', '5', 'PM', '.', 'FYI', ',', 'I\'ll', 'grab', 'some', 'food', 'on', 'the', 'way']
-
-# cleaned words b
-cleaned_words = handling_useless_tokens(word_tokens_en)
-print(cleaned_words)
-
-# replaced abbreviations d
-replace_abbreviations = replace_abbreviations_in_text(cleaned_words)
-print(replace_abbreviations)
-
-# correct misspelled words e
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models/pipeline_pnl.py
@@ -0,0 +1,21 @@
+from .handling_useless_tokens import handling_useless_tokens
+from .replace_abbreviations import replace_abbreviations_in_text
+
+def pipeline_pln(route):
+    print(route)
+    # data scraping a
+
+    # cleaned words stiles b
+
+    # tokenizing sentences and word tokens c
+    word_tokens_en = ['I\'m', 'heading', 'to', 'the', 'gym', 'ASAP', ',', 'but', 'I\'ll', 'be', 'back', 'before', '5', 'PM', '.', 'FYI', ',', 'I\'ll', 'grab', 'some', 'food', 'on', 'the', 'way']
+
+    # cleaned words b
+    cleaned_words = handling_useless_tokens(word_tokens_en)
+    print(cleaned_words)
+
+    # replaced abbreviations d
+    replace_abbreviations = replace_abbreviations_in_text(cleaned_words)
+    print(replace_abbreviations)
+
+    # correct misspelled words e
